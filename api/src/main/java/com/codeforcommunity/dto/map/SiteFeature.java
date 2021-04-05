da955655b2d2dc7a65149e0763c60cfff9eca689
Aaron Pradhan <12a.pradhan@gmail.com>
2021-04-05 16:19:38 -0400

    DTOs

---
+++ api/src/main/java/com/codeforcommunity/dto/map/SiteFeature.java
@@ -0,0 +1,27 @@
+package com.codeforcommunity.dto.map;
+
+import io.vertx.core.json.JsonObject;
+
+public class SiteFeature {
+    private final String type;
+    private final SiteFeatureProperties properties;
+    private final JsonObject geometry;
+
+    public SiteFeature(String type, SiteFeatureProperties properties, JsonObject geometry) {
+        this.type = type;
+        this.properties = properties;
+        this.geometry = geometry;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public SiteFeatureProperties getProperties() {
+        return properties;
+    }
+
+    public JsonObject getGeometry() {
+        return geometry;
+    }
+}
